de77df9d458224d0addb0a986f989f51bc1ba7cf
Tautvidas <t.9b5as@gmail.com>
2012-05-11 19:07:25 +0300

    Add printing differences between e and f1, e and f2, f2 and f1

--- main.py
+++ main.py
@@ -15,6 +15,38 @@ n = int(argv[2])
 totalWorkers = int(argv[3])
 
 globale = Decimal(0)
+values = {}
+getcontext().prec = 50
+
+def f1(n):
+    one = Decimal(1)
+    try:
+        return (one + one/n) ** n
+    except: # Catch division by zero
+        return 0
+
+def f2(n):
+    myE = Decimal(0)
+    one = Decimal(1)
+    for i in xrange(0, n + 1):
+        myE += one / math.factorial(i)
+        yield myE
+
+f2generator = f2(n)
+reale = Decimal(1).exp()
+
+print '|Tikras e - f1|', '|Tikras e - f2|', '|f2 - f1|'
+for i in xrange(0, n + 1):
+    obj = {}
+    val1 = f1(i)
+    val2 = f2generator.next()
+    obj['f1_error'] = reale - val1
+    obj['f2_error'] = reale - val2
+    obj['f1_f2_difference'] = val2 - val1
+    values[i] = obj
+    print obj['f1_error'], obj['f2_error'], obj['f1_f2_difference']
+
+exit()
 
 def worker(q2, q):
     mye = Decimal(0)
@@ -55,7 +87,8 @@ print (real_e)
 print "Tikslumas: "
 print (real_e - globale)
 
+# Find longest most frequent sequence
 e_str = str(globale)
-seq = max([e_str[i:j] for i in xrange(len(e_str) + 1) for j in xrange(i + 2, len(e_str) + 1)], key=lambda s: e_str.count(s))
+seq = max((e_str[i:j] for i in xrange(len(e_str) + 1) for j in xrange(i + 2, len(e_str) + 1)), key=lambda s: e_str.count(s))
 
 print "Ilgiausia seka yra '%s' ir ji pasikartoja %d kartu" % (seq, e_str.count(seq))
